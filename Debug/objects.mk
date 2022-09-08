d43e9dda8d1217c6520024a5e534062eb8d7538e
leonardo-xompero <leonardo.xompero@studenti.unitn.it>
2022-09-08 14:35:24 +0300

    ignore gitignore

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-T"C:/Users/inca/AppData/Local/Energia15/packages/energia/hardware/msp432r/5.29.0/system/energia/linker.cmd" -lmsp432r_MSP-EXP432P401R_EduBPMKII_Screen -lmsp432r_MSP-EXP432P401R_core -l:msp432p4xx_driverlib.a -lstdc++ -lgcc -lm -lnosys -lc C:/ti/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib
-
